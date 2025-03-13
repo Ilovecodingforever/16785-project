646e8764c3b78e1796ab46e54a88c095daf7e48a
Ilovecodingforever <mingzliu@umich.edu>
2025-03-13 17:21:23 -0400

    llm

--- main.py
+++ main.py
@@ -1,15 +1,65 @@
 import streamlit as st
 # from pylips.speech import RobotFace
 # from script_reading import read
+import os
+os.environ['HF_HOME'] = '/home/scratch/mingzhul/.cache/huggingface'
 
-def speak(text):
-    face.say(text)
+# from pylips.speech import RobotFace
+# from pylips.face import FacePresets
+
+import torch
+from transformers import pipeline
+
+
+
+
+# def speak(text):
+#     face.say(text)
+
+
+# # @st.cache_resource
+# def init():
+#     face = RobotFace()
+#     return face
 
 
 # @st.cache_resource
-def init():
-    face = RobotFace()
-    return face
+# def generate_text(messages="Who are you?"):
+#     messages = [
+#         {"role": "user", "content": messages},
+#     ]
+#     # pipe = pipeline("text-generation", model="NousResearch/Hermes-3-Llama-3.2-3B")
+#     # pipe = pipeline("text-generation", model="SweatyCrayfish/llama-3-8b-quantized", device_map='auto')
+#     # model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+#     # pipe = pipeline(
+#     #     "text-generation",
+#     #     model=model_id,
+#     #     model_kwargs={"torch_dtype": torch.bfloat16},
+#     #     device_map="auto",
+#     # )
+#     # pipe = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3", device_map='auto')
+#     # pipe = pipeline("text-generation", model="MaziyarPanahi/Mistral-7B-Instruct-v0.3-GGUF")
+#     pipe = pipeline("text-generation", model="Qwen/Qwen2.5-7B-Instruct", device_map='auto')
+#     print(messages)
+#     print(pipe(messages))
+
+#     return pipe(messages)[0]['generated_text'][1]['content']
+
+
+
+
+
+def generate_text(messages="Who are you?"):
+    messages = [
+        {"role": "user", "content": messages},
+    ]
+
+    pipe = pipeline("text-generation", model="Qwen/Qwen2.5-7B-Instruct", device_map='auto')
+    print(messages)
+    print(pipe(messages))
+
+    return pipe(messages)[0]['generated_text'][1]['content']
+
 
 
 # import os
@@ -22,10 +72,11 @@ if st.button("speak"):
     
     # script = '''FARQUAAD: I've tried to be fair to you. Now my patience has reached its end. Tell me or I'll...'''
     # read(script)
-    
+    reply = generate_text(text)
+
     # write to a file
     with open("script.txt", "w") as file:
-        file.write(text)
+        file.write(reply)
     
     # exec(open("script_reading.py").read())
     
--- script_reading.py
+++ script_reading.py
@@ -2,7 +2,71 @@ from pylips.speech import RobotFace
 from pylips.face import FacePresets
 
 
-# import torch
+# read the script
+with open("script.txt", "r") as file:
+    reply = file.read()
+
+
+face = RobotFace()
+face.say(reply)
+
+
+
+
+
+pass
+
+# generate_text()
+
+# messages = [
+#     {"role": "user", "content": "Who are you?"},
+# ]
+# pipe = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3")
+# pipe(messages)
+
+# from transformers import pipeline
+
+# messages = [
+#     {"role": "user", "content": "Who are you?"},
+# ]
+# pipe = pipeline("text-generation", model="google-t5/t5-small")
+# pipe(messages)
+
+
+    # pipe = pipeline("text-generation", model="NousResearch/Hermes-3-Llama-3.2-3B")
+    # pipe = pipeline("text-generation", model="SweatyCrayfish/llama-3-8b-quantized", device_map='auto')
+    # model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+    # pipe = pipeline(
+    #     "text-generation",
+    #     model=model_id,
+    #     model_kwargs={"torch_dtype": torch.bfloat16},
+    #     device_map="auto",
+    # )
+    # pipe = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3", device_map='auto')
+    # pipe = pipeline("text-generation", model="MaziyarPanahi/Mistral-7B-Instruct-v0.3-GGUF")
+
+
+
+# actors = {
+#         'FARQUAAD': RobotFace(robot_name='FARQUAAD', voice_id="com.apple.voice.compact.en-GB.Daniel"),
+#     }
+
+# actors['FARQUAAD'].set_appearance(FacePresets.chili)
+
+# for line in script.split('\n'):
+#     if ':' not in line:
+#         continue
+
+#     actor, content = line.split(':')
+#     face = actors[actor]
+#     face.say(content)
+#     face.wait()
+
+
+
+
+
+
 # from transformers import AutoModel, AutoModelForCausalLM
 # from optimum.bettertransformer import BetterTransformer
 # model = AutoModelForCausalLM.from_pretrained("gpt2-large", torch_dtype=torch.float16)
@@ -29,57 +93,3 @@ from pylips.face import FacePresets
 # input_ids = tokenizer("respond to this: how are you", return_tensors="pt").input_ids
 # outputs = model.generate(input_ids)
 # print(tokenizer.decode(outputs[0], skip_special_tokens=True))
-
-
-from transformers import pipeline
-
-messages = [
-    {"role": "user", "content": "Who are you?"},
-]
-pipe = pipeline("text-generation", model="NousResearch/Hermes-3-Llama-3.2-3B")
-print(messages)
-print(pipe(messages))
-
-
-messages = [
-    {"role": "user", "content": "Who are you?"},
-]
-pipe = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3")
-pipe(messages)
-
-from transformers import pipeline
-
-messages = [
-    {"role": "user", "content": "Who are you?"},
-]
-pipe = pipeline("text-generation", model="google-t5/t5-small")     
-pipe(messages)
-
-
-# read the script
-with open("script.txt", "r") as file:
-    script = file.read()
-
-
-face = RobotFace()
-face.say(script)
-
-
-
-
-pass
-
-# actors = {
-#         'FARQUAAD': RobotFace(robot_name='FARQUAAD', voice_id="com.apple.voice.compact.en-GB.Daniel"),
-#     }
-
-# actors['FARQUAAD'].set_appearance(FacePresets.chili)
-
-# for line in script.split('\n'):
-#     if ':' not in line:
-#         continue
-
-#     actor, content = line.split(':')
-#     face = actors[actor]
-#     face.say(content)
-#     face.wait()
