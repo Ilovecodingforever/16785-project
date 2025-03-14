d557e677d987a134491a5f12e94adf31c7c63799
Ilovecodingforever <mingzliu@umich.edu>
2025-03-14 15:55:59 -0400

    clean up

--- main.py
+++ main.py
@@ -9,17 +9,14 @@ import streamlit as st
 
 
 
-
-def generate_text(messages):
-    pipe = pipeline("text-generation", model="Qwen/Qwen2.5-7B-Instruct", device='auto')
-    print(messages)
-    print(pipe(messages))
-    return pipe(messages)[0]['generated_text'][1]['content']
-
-
-
 if 'messages' not in st.session_state:
     st.session_state.messages = []
+if 'pipe' not in st.session_state:
+    st.session_state.pipe = pipeline("text-generation", model="Qwen/Qwen2.5-7B-Instruct", device_map='auto')
+
+
+
+
 
 text = st.text_input("text", )
 
@@ -27,10 +24,10 @@ text = st.text_input("text", )
 if st.button("speak"):
     st.session_state.messages.append({"role": "user", "content": text})
 
-    reply = generate_text(st.session_state.messages)
+    reply = st.session_state.pipe(st.session_state.messages)[0]['generated_text'][1]['content']
     st.session_state.messages.append({"role": "robot", "content": reply})
 
     # write to a file
     with open("script.txt", "w") as file:
         file.write(reply)
-    
+
