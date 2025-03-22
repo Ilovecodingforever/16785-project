086b7b30c90e40e4c3d2e9aa51b3ad28f5126e70
Ilovecodingforever <mingzliu@umich.edu>
2025-03-21 21:46:42 -0400

    emotion

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ import torch
 from transformers import pipeline
 
 import streamlit as st
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 
 
 if 'messages' not in st.session_state:
     st.session_state.messages = []
-if 'pipe' not in st.session_state:
-    st.session_state.pipe = pipeline("text-generation", model="Qwen/Qwen2.5-7B-Instruct", device_map='auto')
-
+if 'pipe_generate' not in st.session_state:
+    # st.session_state.model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-7B-Instruct")
+    # st.session_state.pipe_generate = pipeline("text-generation", st.session_state.model, 
+    st.session_state.pipe_generate = pipeline("text-generation", model="Qwen/Qwen2.5-7B-Instruct", 
+                                              device_map='auto', max_length=512)
+    # st.session_state.pipe_classify = pipeline("text-classification", model="Qwen/Qwen2.5-7B-Instruct", 
+    #                                           device_map='auto', max_length=512)
 
 
 
@@ -24,10 +29,41 @@ text = st.text_input("text", )
 if st.button("speak"):
     st.session_state.messages.append({"role": "user", "content": text})
 
-    reply = st.session_state.pipe(st.session_state.messages)[0]['generated_text'][1]['content']
+    # tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-7B-Instruct")
+    # pipe = pipeline("text-generation", st.session_state.model, tokenizer=tokenizer,
+                    # device_map='auto', max_length=512)
+    reply = st.session_state.pipe_generate(st.session_state.messages)
+    # reply = pipe(st.session_state.messages)
+    print(reply)
+    reply = reply[-1]['generated_text'][-1]['content']
     st.session_state.messages.append({"role": "robot", "content": reply})
+    
+    # st.session_state.model.to('cpu')
+    
+    emotion = st.session_state.pipe_generate(
+        [{"role": "user", "content": 
+            "classify this message and do not explain, is it happy, sad, surprise, fear, angry, or disgust: " + reply}]
+                                             )[-1]['generated_text'][-1]['content']
+    print(emotion)
+    if 'happy' in emotion:
+        emotion = 'happy'
+    elif 'sad' in emotion:
+        emotion = 'sad'
+    elif 'surprise' in emotion:
+        emotion = 'surprise'
+    elif 'fear' in emotion:
+        emotion = 'fear'
+    elif 'angry' in emotion:
+        emotion = 'angry'
+    elif 'disgust' in emotion:
+        emotion = 'disgust'
+    else:
+        emotion = 'default'
 
     # write to a file
     with open("script.txt", "w") as file:
         file.write(reply)
+        file.write("\n")
+        file.write(emotion)
+        file.write("\n")
 
--- script_reading.py
+++ script_reading.py
@@ -1,5 +1,6 @@
 from pylips.speech import RobotFace
 from pylips.face import FacePresets
+from pylips.face import ExpressionPresets
 
 
 
@@ -53,9 +54,26 @@ read_from_ssh()
 
 # read the script
 with open("script.txt", "r") as file:
-    reply = file.read()
+    reply = file.readlines()
 
+emotion = reply[1]
+reply = reply[0]
 
 face = RobotFace()
+if 'happy' in emotion:
+    face.express(ExpressionPresets.happy, 1000)
+elif 'sad' in emotion:
+    face.express(ExpressionPresets.sad, 1000)
+elif 'surprise' in emotion:
+    face.express(ExpressionPresets.surprise, 1000)
+elif 'fear' in emotion:
+    face.express(ExpressionPresets.fear, 1000)
+elif 'angry' in emotion:
+    face.express(ExpressionPresets.angry, 1000)
+elif 'disgust' in emotion:
+    face.express(ExpressionPresets.disgust, 1000)
+else:
+    face.express(ExpressionPresets.default, 1000)
+
 face.say(reply)
 
